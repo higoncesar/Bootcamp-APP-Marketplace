84eea19482a4c3d22904a0578c32f866f551f079
Higon César <higon_cs@hotmail.com>
2019-05-25 16:31:25 -0300

    Autenticacao JWT

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const bcrytp = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../../config/auth')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -29,5 +31,19 @@ UserSchema.pre('save', async function (next) {
 
   this.password = await bcrytp.hash(this.password, 8)
 })
+
+UserSchema.methods = {
+  compareHas (password) {
+    return bcrytp.compare(password, this.password)
+  }
+}
+
+UserSchema.statics = {
+  generateToken ({ id }) {
+    return jwt.sign({ id }, authConfig.secret, {
+      expiresIn: authConfig.ttl
+    })
+  }
+}
 
 module.exports = mongoose.model('User', UserSchema)
